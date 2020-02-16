659874c96b5b976fe40fef051dd6b12a46a65696
KovalenkoAV <merlinwt@gmail.com>
2020-02-16 16:12:44 +0300

    dev tools fro redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux"
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const store = (window.devToolsExtension
+    ? window.devToolsExtension()(createStore)
+    : createStore)(rootReducer);
 
 export default store;
